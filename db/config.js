e6d98a0879f00eef1d9ff6b83faa00e7507e2b72
Edy picazo <Edy picazo>
2017-01-11 18:41:41 -0600

    Heroku config

--- db/config.js
+++ db/config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/recargas');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/recargas');
 
 
 mongoose.connection.on('error', err => {
